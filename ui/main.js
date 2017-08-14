2d95adb83ac4bd1175ff856f456ad2a4b330cea0
Shreya Singh <singh.shreya8@gmail.com>
2017-08-14 22:07:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,16 @@
-
+console.log("Loaded!");
 var element = document.getElementById('main-text');
 element.innerHTML ='Hello! I am a webapp of Shreya Singh';
 var img= document.getElementById('madi');
 img.onclick=function(){
     img.style.marginLeft='100px';
 };
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft =marginLeft+"px";
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,100);
+}
