25ab04b8b3d106474e44672ad965b1b15b94fb38
Shreya Singh <singh.shreya8@gmail.com>
2017-08-14 21:46:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getElementById('main-text');
+var element= document.getElementById('main-text');
 element.innerHTML='Hello! I am a webapp of Shreya Singh';
