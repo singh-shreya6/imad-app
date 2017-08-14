e8d4fb0d7824e35d5938f484956aaaba4f65cd9f
Shreya Singh <singh.shreya8@gmail.com>
2017-08-14 23:43:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,14 @@
-var button= document.getElementById('counter');
+var button= document.getElementById("counter");
 button.onclick = function(){
  
 var request=new XMLHttpRequest();
 request.onreadystatechange= function()
 {
-    if(request.readystate===XMLHttpRequest.DONE)
+    if(request.readystate === XMLHttpRequest.DONE)
     {
         if(request.status===200){
         var counter=request.responseText;
-        var span= document.getElementById('count');
+        var span= document.getElementById("count");
         span.innerHTML=counter.toString();
     }
     }
