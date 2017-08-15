34840de7233e2fb3cfd7d527ba44040ef82c9f62
Shreya Singh <singh.shreya8@gmail.com>
2017-08-16 01:38:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,13 +7,14 @@ request.onreadystatechange= function()
 {
     if(request.readyState === XMLHttpRequest.DONE)
     {
-        if(request.status===200){
+        if(request.status===200)
+        {
         var counter=request.responseText;
         var span= document.getElementById('count');
         span.innerHTML=counter.toString();
+         }
     }
-    }
-};
+     };
  
  request.open('GET', 'http://singhshreya8.imad.hasura-app.io/counter',true);
  request.send(null);
@@ -30,7 +31,8 @@ submit.onclick=function()
     {
         if(request.readyState === XMLHttpRequest.DONE)
         {
-            if(request.status===200){
+            if(request.status===200)
+            {
                 var names=request.responseText;
                 names=JSON.parse(names);
                   var list='';
@@ -41,8 +43,8 @@ submit.onclick=function()
                   var ul=document.getElementById('namelist');
                   ul.innerHTML=list;
             
-     }
-    }
+                }
+        }
 };
  var nameInput=document.getElementById('name');
  var name=nameInput.value;
