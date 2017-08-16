923bec7f7c4e59f7977167e6b246b462352cb068
Shreya Singh <singh.shreya8@gmail.com>
2017-08-16 14:17:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 var counter=0;
 app.get('/counter' ,function (req,res){
     counter=counter + 1;
