0174dbc2e98b53d117f8d7a6bfe868b9afc0c9cc
Shreya Singh <singh.shreya8@gmail.com>
2017-08-19 16:43:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,38 +35,7 @@ var articles={
         
     }
 };
-function createTemplate(data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-    var htmlTemplate=`<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet"/>
-    </head>
-    <body>
-        <div class="container">
-            <div>
-                <a href="/">Home</a>
-            </div>
-            <br/>
-             ${heading}
-            <div>
-                ${date.toDateString()}
-            </div>
-            <div>
-              ${content}
-            </div>
-        </div>
-    </body>
-    </html>`
-    ;
-    return htmlTemplate;
-}
+
 var app = express();
 app.use(morgan('combined'));
 
