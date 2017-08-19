44daf6d9a16da8a301facd4795b4a1d6622eee54
Shreya Singh <singh.shreya8@gmail.com>
2017-08-19 16:46:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,27 +14,38 @@ var config={
 var app = express();
 app.use(morgan('combined'));
 
-var articles={
-    'article-one':{
-    title:'article-one/Shreya Singh',
-    heading:'Article one',
-    date: 'August 13,2017',
-    content:'<p>This is the content for my first article </p>'
-    },
-    'article-two':{
-    title:'article-two/Shreya Singh',
-    heading:'Article two',
-    date:'August 13,2017',
-    content:'<p>This is the content for my second article </p>'
-     },
-    'article-three':{
-    title:'article-three/Shreya Singh',
-    heading:'Article three',
-    date:'August 13,2017',
-    content:'<p>This is the content for my third article </p>'
-        
-    }
-};
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var htmlTemplate=`<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <br/>
+             ${heading}
+            <div>
+                ${date.toDateString()}
+            </div>
+            <div>
+              ${content}
+            </div>
+        </div>
+    </body>
+    </html>`
+    ;
+    return htmlTemplate;
+}
 
 var app = express();
 app.use(morgan('combined'));
